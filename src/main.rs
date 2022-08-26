6c45c3c20501a2586f64cc9dfa68436b62641bfa
Nathan Stocks <cleancut@github.com>
2022-08-25 21:05:25 -0600

    got the sprite sheets working

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use bevy::prelude::{App, ClearColor, Color, Msaa, NonSend, WindowDescriptor};
+use bevy::render::texture::ImageSettings;
 use bevy::window::WindowId;
 use bevy::winit::WinitWindows;
 use bevy::DefaultPlugins;
@@ -13,6 +14,7 @@ fn main() {
     App::new()
         .insert_resource(Msaa { samples: 1 })
         .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
+        .insert_resource(ImageSettings::default_nearest())
         .insert_resource(WindowDescriptor {
             width: 1280.,
             height: 720.,
@@ -22,12 +24,12 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(GamePlugin)
-        //.add_startup_system(set_window_icon)
+        .add_startup_system(set_window_icon)
         .run();
 }
 
 // Sets the icon on windows and X11
-/* fn set_window_icon(windows: NonSend<WinitWindows>) {
+fn set_window_icon(windows: NonSend<WinitWindows>) {
     let primary = windows.get_window(WindowId::primary()).unwrap();
     let icon_buf = Cursor::new(include_bytes!("../assets/textures/bevy.png"));
     if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
@@ -38,4 +40,3 @@ fn main() {
         primary.set_window_icon(Some(icon));
     };
 }
- */
--- src/player.rs
+++ src/player.rs
@@ -12,16 +12,30 @@ pub struct Player;
 /// Player logic is only active during the State `GameState::Playing`
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system_set(SystemSet::on_enter(GameState::Playing).with_system(spawn_player))
-            .add_system_set(SystemSet::on_update(GameState::Playing).with_system(move_player));
+        app.insert_resource(MouseLocation(Vec2::ZERO))
+            .add_system_set(SystemSet::on_enter(GameState::Playing).with_system(spawn_player))
+            .add_system_set(SystemSet::on_update(GameState::Playing).with_system(move_player))
+            .add_system_set(SystemSet::on_update(GameState::Playing).with_system(change_sprite))
+            .add_system(update_mouse_location);
     }
 }
 
-fn spawn_player(mut commands: Commands, textures: Res<TextureAssets>) {
+fn spawn_player(
+    mut commands: Commands,
+    textures: Res<TextureAssets>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_atlas =
+        TextureAtlas::from_grid(textures.materials_sheet.clone(), Vec2::splat(16.0), 10, 25);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
     commands
-        .spawn_bundle(SpriteBundle {
-            texture: textures.texture_bevy.clone(),
-            transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
+        .spawn_bundle(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform {
+                translation: Vec3::new(0., 0., 1.),
+                scale: Vec3::splat(4.0),
+                ..Default::default()
+            },
             ..Default::default()
         })
         .insert(Player);
@@ -31,17 +45,61 @@ fn move_player(
     time: Res<Time>,
     actions: Res<Actions>,
     mut player_query: Query<&mut Transform, With<Player>>,
+    mouse_location: ResMut<MouseLocation>,
 ) {
-    if actions.player_movement.is_none() {
+    // if actions.player_movement.is_none() {
+    //     return;
+    // }
+    // let speed = 150.;
+    // let movement = Vec3::new(
+    //     actions.player_movement.unwrap().x * speed * time.delta_seconds(),
+    //     actions.player_movement.unwrap().y * speed * time.delta_seconds(),
+    //     0.,
+    // );
+    for mut player_transform in &mut player_query {
+        player_transform.translation = mouse_location.0.extend(0.0);
+    }
+}
+
+fn change_sprite(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut TextureAtlasSprite, &Handle<TextureAtlas>), With<Player>>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+) {
+    for (mut sprite, texture_atlas_handle) in &mut query {
+        let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+        if keyboard_input.just_pressed(KeyCode::Left) {
+            sprite.index = if sprite.index > 0 {
+                sprite.index - 1
+            } else {
+                texture_atlas.textures.len() - 1
+            };
+        }
+        if keyboard_input.just_pressed(KeyCode::Right) {
+            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct MouseLocation(Vec2);
+
+fn update_mouse_location(
+    mut cursor_moved_events: EventReader<CursorMoved>,
+    mut mouse_location: ResMut<MouseLocation>,
+    windows: Res<Windows>,
+) {
+    // Get window dimensions
+    // It's possible to not have window dimensions for the first frame or two
+    let window_dimensions;
+    if let Some(window) = windows.get_primary() {
+        window_dimensions = Vec2::new(window.width(), window.height());
+    } else {
         return;
     }
-    let speed = 150.;
-    let movement = Vec3::new(
-        actions.player_movement.unwrap().x * speed * time.delta_seconds(),
-        actions.player_movement.unwrap().y * speed * time.delta_seconds(),
-        0.,
-    );
-    for mut player_transform in &mut player_query {
-        player_transform.translation += movement;
+
+    // Update the mouse location
+    if let Some(event) = cursor_moved_events.iter().last() {
+        mouse_location.0 = event.position - (window_dimensions * 0.5);
     }
 }
